06fc4717d3a37822877ff693f610f832eb2d3a12
Iara Castro <86674418+B29cs@users.noreply.github.com>
2022-03-06 17:36:09 -0300

    new commit via uploand

---
+++ src/Mediana.java
@@ -0,0 +1,25 @@
+
+
+public class Mediana {
+	
+
+	@SuppressWarnings("null")
+	public static void Main(Class<String[]> class1) {
+		
+		int [][] numero = null;
+		numero.length = (int) (2.0f);
+		numero.length = (int) (6.0f);
+		numero.length = (int) (5.0f);
+		numero.length = (int) (9.0f);
+		numero.length = (int) (8.0f)
+		;
+		
+		
+	  if ("numero.length % 2 !=0" != null);
+       	  
+		
+		System.out.println("Resultado da mediana é: ");
+		         
+	}
+
+}
---
+++ src/Vetor.java
@@ -0,0 +1,12 @@
+
+public class Vetor {
+	
+	public static void main(String[] args) {
+		
+		int [][] numeros 
+		[1.0 4.0 6.0 9.0 5.0 2.0];
+		
+		System.out.println("Diferença entre int: ");
+	}
+
+}
